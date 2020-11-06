7001dab8965b62884fdf53222ea22206ce4533aa
sakeena81 <71390984+sakeena81@users.noreply.github.com>
2020-11-06 15:00:43 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -32,31 +32,44 @@ function deleteRecord(button){
 }
 
 }
+function save(button){
+    document.getElementById("editperson").setAttribute("hidden","hidden");
+}
+function editrecord(button) { 
+    //unhide the form, 
+    //get the record from the button we clicked
+    //populate the form, 
 
-function editRecord(id) {
+    document.getElementById("editperson").removeAttribute("hidden");
+var test = button.id;
 
-    document.getElementById("addNI").style.visibility="visible";
-    document.getElementById("addFullName").style.visibility="visible";
-    document.getElementById("addPhone").style.visibility="visible";
-    document.getElementById("addAdd").style.visibility="visible";
-    document.getElementById("addDep").style.visibility="visible";
+for (var i = 0; i < records.length; i++){
+
+if (records[i].ninumber = button.id){
+    document.getElementById("editNI").value = records[i].ninumber;
+    document.getElementById("editName").value =records[i].fullname;
+    document.getElementById("editPhone").value =records[i].phone;
+    document.getElementById("editAddress").value =records[i].address;
+    document.getElementById("editDept").value =records[i].department;
+}
+}
+
+    // //saveChanges = document.createElement("input");
+    // saveChanges.type="button";
+    // saveChanges.id = "save";
+    // saveChanges.value = "Save Changes";
+    // saveChanges.onclick=()=> confirmEdit(id);
+    // let newButton = document.getElementById("editButt");
+    // newButton.innerHTML = "";
+    // newButton.appendChild(saveChanges);
+
+    //document.getElementById("addNI").value = records[id].ninumber;
+    //document.getElementById("addFullName").value = records[id].fullname;
+    //document.getElementById("addPhone").value = records[id].phone;
+    //document.getElementById("addAdd").value = records[id].address;
+    //document.getElementById("addDep").value = records[id].department;
     
-    saveChanges = document.createElement("input");
-    saveChanges.type="button";
-    saveChanges.id = "save";
-    saveChanges.value = "Save Changes";
-    saveChanges.onclick=()=> confirmEdit(id);
-    let newButton = document.getElementById("editButt");
-    newButton.innerHTML = "";
-    newButton.appendChild(saveChanges);
-
-    document.getElementById("addNI").value = records[id].ninumber;
-    document.getElementById("addFullName").value = records[id].fullname;
-    document.getElementById("addPhone").value = records[id].phone;
-    document.getElementById("addAdd").value = records[id].address;
-    document.getElementById("addDep").value = records[id].department;
-    
-    }
+}
 
 function confirmEdit(id) {
     
